25c0c4f80bc6c5e36e6fffea09bcd82eb9bf9b40
Dmitriy Shishkin <4144442@bk.ru>
2021-09-22 20:57:44 +0300

    MyCommit On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	new file:   README.txt
    	new file:   src/.Solution.java.swp
    	modified:   src/Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -2,6 +2,10 @@ import org.apache.log4j.Appender;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
 
+//добавил комментарий
+//еще добавил коментарий
+//+ еще один комментарий
+
 public class Solution extends AppenderSkeleton {
     @Override
     protected void append(LoggingEvent loggingEvent) {
