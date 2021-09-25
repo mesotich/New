7522c180c93c6c456dc2f79a5b22d5f382556acb
Dmitriy Shishkin <4144442@bk.ru>
2021-09-25 19:36:03 +0300

    Commit

--- src/Solution.java
+++ src/Solution.java
@@ -2,6 +2,9 @@ import org.apache.log4j.Appender;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution extends AppenderSkeleton {
     @Override
     protected void append(LoggingEvent loggingEvent) {
@@ -9,6 +12,8 @@ public class Solution extends AppenderSkeleton {
 
     @Override
     public void close() {
+        Map<Integer,String> map = new HashMap<>();
+
     }
 
     @Override
