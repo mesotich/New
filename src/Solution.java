4c4817cbd013ea039b74896744888dfc06716a8c
Dmitriy Shishkin <4144442@bk.ru>
2021-09-25 23:54:42 +0300

    ClassForGeneric

--- src/Solution.java
+++ src/Solution.java
@@ -3,15 +3,17 @@
 Несколько суперклассов для дженерика
 */
 
-public class Solution<T> {
+import java.util.List;
+
+public class Solution<T extends ClassForGenerics & InterfaceForGenerics> {
     public static void main(String[] args) {
         Solution<TestClassGood> testClassSolution = new Solution<>();
         testClassSolution.check();
 //!!! Следующие оба варианта не должны работать, закомментируй их:
-        Solution<TestClassWrong1> wrong1Solution = new Solution<>();
-        wrong1Solution.check();
-        Solution<TestClassWrong2> wrong2Solution = new Solution<>();
-        wrong2Solution.check();
+        //Solution<TestClassWrong1> wrong1Solution = new Solution<>();
+        //wrong1Solution.check();
+        //Solution<TestClassWrong2> wrong2Solution = new Solution<>();
+        //wrong2Solution.check();
     }
 
     public void check() {
