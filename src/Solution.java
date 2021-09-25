245a1db2b192577deefad3f93c1ceb990c49a4b8
Dmitriy Shishkin <4144442@bk.ru>
2021-09-25 21:14:31 +0300

    SomeClass done

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution<T extends Number, L extends List<Solution.SomeClass<T>>> {
+public class Solution<L extends List<Solution.SomeClass>> {
     public static class SomeClass<T extends Number> {
-        private T t;
-
-        public SomeClass(T t) {
-            this.t = t;
-        }
-
-        public void print() {
-            System.out.println("T = " + t.getClass().getSimpleName());
-        }
     }
 
     public static void main(String[] args) {
-        Solution<Integer,ArrayList<Solution.SomeClass<Integer>>> solution = new Solution();
+
     }
 }
