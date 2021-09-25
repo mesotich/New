7f1ce7d2782193f47b4ccf4df3e1a349bbe47eb9
Dmitriy Shishkin <4144442@bk.ru>
2021-09-25 20:41:38 +0300

    SomeClass with main

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,20 @@
-public class Solution {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution<T extends Number, L extends List<Solution.SomeClass<T>>> {
+    public static class SomeClass<T extends Number> {
+        private T t;
+
+        public SomeClass(T t) {
+            this.t = t;
+        }
+
+        public void print() {
+            System.out.println("T = " + t.getClass().getSimpleName());
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution<Integer,ArrayList<Solution.SomeClass<Integer>>> solution = new Solution();
+    }
 }
