d16e4df8daa37520b04107cf519c61a571d582c6
Dmitriy Shishkin <4144442@bk.ru>
2021-09-27 01:08:10 +0300

    Convertable v2

--- src/ConvertableUtil.java
+++ src/ConvertableUtil.java
@@ -1,14 +1,9 @@
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class ConvertableUtil {
     public static <K, V extends Convertable<K>> Map<K, V> convert(List<V> list) {
-        Map<K, V> result = new HashMap<>();
-        list.forEach(i -> result.put(i.getKey(), i));
-        return result;
+        return list.stream().collect(Collectors.toMap(Convertable::getKey, x->x));
     }
 }
