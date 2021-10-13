dfbe0c940fce34b7ae55d9d1edfa75a6d6715db6
Dmitriy Shishkin <4144442@bk.ru>
2021-10-13 21:11:08 +0300

    List

--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -1,34 +1,48 @@
 package solution;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+
 /*
-Рефакторинг MVC
+Найти класс по описанию Ӏ Java Collections: 6 уровень, 6 лекция
 */
 
 public class Solution {
     public static void main(String[] args) {
-// Fetch car record from the database
-        CarModel model = retrieveCarFromDatabase();
-// Create a view to show the car's speed on a speedometer (the console)
-        SpeedometerView view = new SpeedometerView();
-        CarController controller = new CarController(model, view);
-        controller.updateView();
-// Update the model data
-        controller.increaseSpeed(15);
-        controller.updateView();
-// Update the model data
-        controller.increaseSpeed(50);
-        controller.updateView();
-// Update the model data
-        controller.decreaseSpeed(7);
-        controller.updateView();
+        System.out.println(getExpectedClass());
     }
 
-    private static CarModel retrieveCarFromDatabase() {
-        CarModel currentCar = new CarModel();
-        currentCar.setBrand("Nissan");
-        currentCar.setModel("Almera Classic");
-        currentCar.setSpeed(0);
-        currentCar.setMaxSpeed(200);
-        return currentCar;
+    public static Class<?> getExpectedClass() {
+        List<Class<?>> classList = new ArrayList<>(Arrays.asList(Collections.class.getDeclaredClasses()));
+        for (int i = 0; i < classList.size(); i++) {
+            Class<?>[] classes = classList.get(i).getDeclaredClasses();
+            if (classes.length != 0) {
+                classList.addAll(Arrays.asList(classes));
+            }
+        }
+        classList = classList.stream()
+                .filter(List.class::isAssignableFrom)
+                .filter(a -> Modifier.isPrivate(a.getModifiers()))
+                .filter(a -> Modifier.isStatic(a.getModifiers()))
+                .collect(Collectors.toList());
+        for (Class<?> cl : classList
+        ) {
+            try {
+                Method method = cl.getMethod("get", int.class);
+                method.setAccessible(true);
+                Constructor<?> constructor = cl.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                method.invoke(constructor.newInstance(), 1);
+            } catch (Exception e) {
+                if (e.getCause() instanceof IndexOutOfBoundsException)
+                    return cl;
+            }
+        }
+        return null;
     }
 }
