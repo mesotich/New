c7d36039ca4cb888c7f2ad781913d257b37da55a
Dmitriy Shishkin <4144442@bk.ru>
2021-11-08 14:56:19 +0300

    RBtree

--- src/solution/RedBlackTree.java
+++ src/solution/RedBlackTree.java
@@ -31,25 +31,15 @@ public class RedBlackTree {
 
     public void insert(int item) {
         current = grand = parent = header;
-        System.out.println("current = " + current.element + " current color = " + current.color);
         EMPTY.element = item;
         while (current.element != item) {
             great = grand;
-            System.out.println("great = " + great.element + " great color = " + great.color);
             grand = parent;
-            System.out.println("grand = " + grand.element + " grand color = " + grand.color);
             parent = current;
-            System.out.println("parent = " + parent.element + " parent color = " + parent.color);
             current = item > current.element ? current.right : current.left;
-            System.out.println("current = " + current.element + " current color = " + current.color);
-            if (current.left.color == Color.BLACK && current.right.color == Color.BLACK) {
-                System.out.println("parent before1 = " + parent.element+ " parent color1 = " + parent.color);
-                System.out.println("current before1 = " + current.element+ " current color1 = " + current.color);
+            if (current.left.color == Color.RED && current.right.color == Color.RED) {
                 reorient(item);
-                System.out.println("parent after1 = " + parent.element+ " parent color1 = " + parent.color);
-                System.out.println("current after1 = " + current.element+ " current color1 = " + current.color);
             }
-            System.out.println("--------------------------");
         }
         if (current != EMPTY) {
             return;
@@ -60,12 +50,7 @@ public class RedBlackTree {
         } else {
             parent.right = current;
         }
-        System.out.println("parent before = " + parent.element+ " parent color = " + parent.color);
-        System.out.println("current before = " + current.element+ " current color = " + current.color);
         reorient(item);
-        System.out.println("parent after = " + parent.element+ " parent color = " + parent.color);
-        System.out.println("current after = " + current.element+ " current color = " + current.color);
-        System.out.println("=================================");
     }
 
     protected void reorient(int item) {
